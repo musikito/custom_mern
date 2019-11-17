1252733a4871ffc9fc9513538d2407504edf2c9b
JoseMiguel Marte <tumusicalatina@gmail.com>
2019-11-17 16:40:52 -0500

    Finished the bill route

--- backend/models/bills.model.js
+++ backend/models/bills.model.js
@@ -5,7 +5,8 @@ const billsSchema = new Schema({
     bill:{
         type: String,
         required: true,
-        trim: true
+        trim: true,
+        unique: true,
     },
     date: {
         type: Date,
--- backend/routes/bills.js
+++ backend/routes/bills.js
@@ -21,6 +21,7 @@ router.route('/add').post((req, res) => {
     const paidby = Date.parse(req.body.date);
     const amount = Number(req.body.amount);
     const description = req.body.description;
+    const planned = req.body.planned;
 
 
 
@@ -30,7 +31,8 @@ router.route('/add').post((req, res) => {
         category,
         paidby,
         amount,
-        description
+        description,
+        planned
     }); //end of payload
 
     // save it to the DB
@@ -41,6 +43,44 @@ router.route('/add').post((req, res) => {
 
 }); // end POST bills
 
+// @route FIND models/bills
+// @desc find bill ID  
+// find the expense by ID
+router.route('/:id').get((req, res) => {
+    Bill.findById(req.params.id)
+      .then(bill=> res.json(bill))
+      .catch(err => res.status(400).json('Error: ' + err));
+  });
 
+  // @route DELETE models/bills
+  // @desc DELETE a bill  
+  // Delete BILL by ID
+  router.route('/:id').delete((req, res) => {
+    Bill.findByIdAndDelete(req.params.id)
+      .then(() => res.json('Bill deleted.'))
+      .catch(err => res.status(400).json('Error: ' + err));
+  });
+ 
+ // @route UPDATE models/bills
+ // @desc UPDATE a bill  
+ // Update expense by ID
+  router.route('/update/:id').post((req, res) => {
+    Bill.findById(req.params.id)
+      .then(bill => {
+        bill.bill = req.body.bill;
+        bill.description = req.body.description;
+        bill.amount= Number(req.body.amount);
+        bill.paidby = Date.parse(req.body.date);
+        bill.date = Date.parse(req.body.date);
+        bill.category = req.body.category;
+        bill.planned = req.body.planned;
+  
+        bill.save()
+          .then(() => res.json('Bill updated!'))
+          
+          .catch(err => res.status(400).json('Error: ' + err));
+      })
+      .catch(err => res.status(400).json('Error: ' + err));
+  });
 
 module.exports = router;
